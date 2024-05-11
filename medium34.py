5215703f926e57ad0a3b018bd8556348140c51be
Ivan Fang <iFang.py@gmail.com>
2024-05-11 15:48:22 +1000

    Update medium34.py

--- medium34.py
+++ medium34.py
@@ -19,6 +19,22 @@ class Solution:
                 if i in zeroes_row or j in zeroes_col:
                     matrix[i][j] = 0
 
-
+# __________
 # Solution 2
-
+def setZeroes(matrix: List[List[int]]) -> None:
+    if not matrix:return []
+    row=len(matrix);col=len(matrix[0])
+    zr=[False]*row;zc=[False]*col
+    for i in range(row):
+        for j in range(col):
+            if matrix[i][j]==0:zr[i]=True;zc[j]=True
+    for i in range(row):
+        for j in range(col):
+            if zr[i] or zc[j]:matrix[i][j] = 0
+if __name__ == '__main__':
+    import json, sys
+    with open('user.out', 'w') as f:
+        for matrix in map(json.loads, sys.stdin):
+            setZeroes(matrix)
+            print(json.dumps(matrix, separators=(',',':')), file=f)
+    sys.exit()
