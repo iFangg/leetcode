e744cb923e38992125aa6f744c0ac96b07849f91
Ivan Fang <iFang.py@gmail.com>
2024-06-22 15:37:33 +1000

    Update daily6.cpp

--- daily6.cpp
+++ daily6.cpp
@@ -56,3 +56,37 @@ public:
         return result;
     }
 };
+
+// Solution 3
+class Solution {
+public:
+    int f(vector<int>& nums, int k) {
+        if(k < 0) return 0;
+        int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int count = 0;
+
+        while(r < n) {
+            sum += (nums[r] % 2);
+
+            while(sum > k) {
+                sum -= (nums[l] % 2);
+                l++;
+            }
+
+            count += (r-l+1);
+            r++;
+        }
+
+        return cnt;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        return (f(nums, k) - f(nums, k - 1));
+    }
+};
