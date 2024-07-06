8cce68ae43c5b5ac0ed0058c1b034683ef3223f9
Ivan Fang <iFang.py@gmail.com>
2024-07-06 22:01:45 +1000

    Create daily18.cpp

---
+++ daily18.cpp
@@ -0,0 +1,16 @@
+// Solution 1
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        // if time is 1x, 3x, 5x n then it's going the original direction
+        // if time is 2x, 4x, 6x n then it's going in the opposite direction
+        // 1 2 3 4 - 5
+        // (1) 2 3 4 3 2 1 | 2 3 4 3 2 1 | 2 3 4 3 ... cycles of n - 1 * 2
+        auto period = (n - 1) * 2;
+        auto t = time % period;
+        if (t < n)
+            return 1 + t;
+        else
+            return 1 + (period - t);
+    }
+};
