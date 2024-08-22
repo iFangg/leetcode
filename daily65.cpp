6ce8262a7ab5394d3e0a1108d74348732e046a85
Ivan Fang <iFang.py@gmail.com>
2024-08-22 21:56:55 +1000

    Update daily65.cpp

--- daily65.cpp
+++ daily65.cpp
@@ -1,11 +1,37 @@
 #include <vector>
 #include <string>
 
+// Solution 1
 class Solution {
 public:
-    int strangePrinter(std::string s) {
-        /*
-        
-        */
+    int strangePrinter(string s) {
+        s_ = s;
+        dp_ = std::vector<std::vector<int>>(s.size(), std::vector<int>(s.size(), -1));
+
+        return find_min_prints(0, s.size() - 1);
     }
+
+private:
+    int find_min_prints(int i, int j) {
+        if (i > j)
+            return 0;
+        
+        if (dp_[i][j] != -1)
+            return dp_[i][j];
+        
+        auto ans = 1 + find_min_prints(i + 1, j);
+        for (auto k = i + 1; k <= j; ++k) {
+            if (s_[k] == s_[i]) {
+                auto better = find_min_prints(i, k - 1) + find_min_prints(k + 1, j);
+                ans = min(ans, better);
+            }
+        }
+
+        dp_[i][j] = ans;
+
+        return dp_[i][j];
+    }
+
+    std::string s_;
+    std::vector<std::vector<int>> dp_;
 };
