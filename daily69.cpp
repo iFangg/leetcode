15392990cc51ffedaeba15acd7c8bbf74b15a564
Ivan Fang <iFang.py@gmail.com>
2024-08-25 14:30:00 +1000

    Create daily69.cpp

---
+++ daily69.cpp
@@ -0,0 +1,28 @@
+// Solution 1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (root == nullptr)
+            return nodes_;
+        
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        nodes_.push_back(root->val);
+
+        return nodes_;
+    }
+
+private:
+    std::vector<int> nodes_;
+};
