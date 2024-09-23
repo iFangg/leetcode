90179fd68a6e25426a87785c7c89b4643d3a8930
Ivan Fang <iFang.py@gmail.com>
2024-09-23 17:21:29 +1000

    Update daily98.cpp

--- daily98.cpp
+++ daily98.cpp
@@ -55,24 +55,45 @@ public:
 class Solution {
 public:
     int minExtraChar(std::string s, std::vector<std::string>& dictionary) {
-        int n = s.size();
+        int n = s.size();  // Get the length of the string 's'
+        
+        // Create a DP array where dp[i] represents the minimum number of extra characters 
+        // required for the substring s[0:i] (i.e., the first i characters of the string)
         std::vector<int> dp(n + 1, 0);
+        
+        // Convert the dictionary into an unordered_set for fast word lookup
         std::unordered_set<std::string> dict(dictionary.begin(), dictionary.end());
 
-        for (int i = 0; i <= n; ++i) {
-            dp[i] = i; // If no words match, all characters are extra
+        // Iterate through all indices of the string 's', from 1 to n
+        for (int i = 1; i <= n; ++i) {
+            
+            // By default, assume the current character at s[i-1] is extra, 
+            // so dp[i] is at least dp[i-1] + 1 (i.e., one more extra character than before).
+            dp[i] = dp[i - 1] + 1;
 
+            // Now, check if any substring ending at position i-1 matches a word in the dictionary.
+            // We do this by trying all possible starting points 'j' from 0 to i-1 and checking 
+            // if the substring s[j:i] is in the dictionary.
             for (int j = 0; j < i; ++j) {
-                if (dict.find(s.substr(j, i - j)) != dict.end()) {
+                
+                // Extract the substring s[j:i] (from index j to i-1)
+                std::string current_substr = s.substr(j, i - j);
+                
+                // Check if this substring exists in the dictionary
+                if (dict.find(current_substr) != dict.end()) {
+                    // If the substring is a valid word, update dp[i].
+                    // dp[i] should be the minimum of its current value and dp[j], because dp[j]
+                    // represents the number of extra characters up to index j, and since s[j:i] is 
+                    // a valid word, we don't add any extra characters for this part.
                     dp[i] = std::min(dp[i], dp[j]);
                 }
             }
         }
 
+        // Return dp[n], which contains the minimum number of extra characters for the entire string 's'
         return dp[n];
     }
 };
-
 
 // Solution 3
 int _ = [](){ std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); return 0; }();
