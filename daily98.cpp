15447a4feb581d59f8bf8013af16767fd9cfc558
Ivan Fang <iFang.py@gmail.com>
2024-09-23 17:16:26 +1000

    Update daily98.cpp

--- daily98.cpp
+++ daily98.cpp
@@ -73,3 +73,32 @@ public:
     }
 };
 
+
+// Solution 3
+int _ = [](){ std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); return 0; }();
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string> &dictionary) {
+        int const n = s.size();
+        std::vector<int> dp(n+1);
+
+        for(int i = 1; i < n+1; ++i) {
+            dp[i] = dp[i-1] + 1;
+            for(std::string const &word: dictionary) {
+                int const m = word.length();
+                if(i >= m) {
+                    bool flag = true;
+                    for(int j = i-m; j < i; ++j) {
+                        if(s[j] != word[j-i+m]) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag) { dp[i] = std::min(dp[i], dp[i-m]); }
+                }
+            }
+        }
+        return dp[n];
+    }
+};
