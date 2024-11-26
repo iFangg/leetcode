a5d1de73c6c4c235c2ee75747ddac5310773ac3f
Ivan Fang <iFang.py@gmail.com>
2024-11-26 21:54:07 +1100

    Create daily162.cpp

---
+++ daily162.cpp
@@ -0,0 +1,22 @@
+// Solution 1
+class Solution {
+    public int findChampion(int n, int[][] edges) {
+        /**
+        bfs
+        return the node that has no incoming edges
+        */
+
+        int[] in_edges = new int[n];
+        for (int[] e : edges) {
+            in_edges[e[1]]++;
+        }
+
+        List<Integer> no_in = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (in_edges[i] == 0)
+                no_in.add(i);
+        }
+
+        return no_in.size() == 1 ? no_in.get(0) : -1;
+    }
+}
