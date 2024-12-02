038b982c642097ee77d9dd4642cc67936168fb55
Ivan Fang <iFang.py@gmail.com>
2024-12-02 16:23:24 +1100

    Create daily128.java

---
+++ daily128.java
@@ -0,0 +1,41 @@
+// Solution 1
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        /*
+        split string into word array
+        match characters until we find the right prefix
+        */
+
+        String[] words = sentence.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            String w = words[i];
+            int w_idx = 0;
+            int s_idx = 0;
+            while (s_idx < searchWord.length() && w_idx < w.length()) {
+                if (w.charAt(w_idx) != searchWord.charAt(s_idx))
+                    break;
+
+                s_idx++;
+                w_idx++;
+            }
+
+            if (s_idx == searchWord.length())
+                return i + 1;
+        }
+
+        return -1;
+    }
+}
+
+
+// Solution 2
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String arr[]=sentence.split(" ");
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].startsWith(searchWord))
+            return i+1;
+        }
+        return -1;
+    }
+}
